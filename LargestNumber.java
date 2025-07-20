d680f3d22fbcc8d08265e2af0855c68d7172f7a9
iam-dionsuby <iam.dionsuby@gmail.com>
2025-07-20 18:42:15 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class LargestNumber {
+    public static void main(String args[]){
+    Scanner sc= new Scanner(System.in);
+    int NUM1,NUM2,LargeNum;
+    System.out.println("Enter the First number:");
+    NUM1=sc.nextInt();
+    System.out.println("Enter the Second number:");
+    NUM2=sc.nextInt();
+    LargeNum=NUM1>NUM2?NUM1:NUM2;
+    System.out.println(LargeNum+" IS THE LARGEST");
+    }
+}
