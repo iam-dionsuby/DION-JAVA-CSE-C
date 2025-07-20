3c4f12939bf9adb6e1a269b20045858b6e5cc566
iam-dionsuby <iam.dionsuby@gmail.com>
2025-07-20 20:07:14 +0530

    Add files via upload

---
+++ addition.java
@@ -0,0 +1,12 @@
+public class addition {
+   
+    public static void main(String args []){
+
+        int firstnum=10;
+        int secondnum=200;
+        System.out.println("The total is" +" "+ (firstnum+secondnum));
+
+    }
+    
+}
+
